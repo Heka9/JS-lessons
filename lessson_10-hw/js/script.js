84216d269a356c13c92d9ec9d26f1c3ce532f54d
Heka9 <90552593+Heka9@users.noreply.github.com>
2023-03-10 20:21:33 +0200

    Add files via upload

--- lessson_10-hw/js/script.js
+++ lessson_10-hw/js/script.js
@@ -1,15 +1,2 @@
 "use strict"
 
-
-const input = document.querySelector(".input")
-const button = document.querySelector(".button")
-const textarea = document.querySelector(".textarea")
-
-
-button.onclick = function () {
-	let inputValue = input.value
-
-	console.log(inputValue);
-
-	textarea.innerHTML = inputValue
-}
